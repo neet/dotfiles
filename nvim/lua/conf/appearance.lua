59dde8ff8fb2ba167d441983e8dddbabafdbda0d
Ryo Igarashi <n33t5hin@gmail.com>
2025-09-29 15:52:51 +0900

    chore: Prettify nvim/lua/conf/appearance.lua

--- nvim/lua/conf/appearance.lua
+++ nvim/lua/conf/appearance.lua
@@ -2,7 +2,7 @@ vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
 
-local github_theme = require("github-theme").setup({
+require("github-theme").setup({
     options = {
         terminal_colors = false,
     },
