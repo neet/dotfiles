53f987760e57ab53419fa2f6c251cf12609cf36c
Ryo Igarashi <n33t5hin@gmail.com>
2025-10-30 21:30:27 +0900

    feat: Use pyslp

--- .config/nvim/lua/conf/lsp.lua
+++ .config/nvim/lua/conf/lsp.lua
@@ -2,6 +2,17 @@
 MiniDeps.now(function()
     vim.lsp.enable('lua_ls')
     vim.lsp.enable('ts_ls')
+
     vim.lsp.enable('ruby_lsp')
+
+    vim.lsp.enable('pylsp', {
+        settings = {
+            pylsp = {
+                plugins = {
+                    ruff = { enabled = true },
+                }
+            }
+        }
+    })
 end)
 
