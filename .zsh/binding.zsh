f201d216bc40d2b29fbfae74d9cfa02463f439d4
Ryo Igarashi <n33t5hin@gmail.com>
2025-12-07 16:43:36 +0900

    fix: Hide current window

--- .zsh/binding.zsh
+++ .zsh/binding.zsh
@@ -1,10 +1,6 @@
 autoload -Uz search_ghq_with_fzf
-autoload -Uz search_tmux 
 
 # Ctrl-g: Search GHQ with Fzf
 zle -N search_ghq_with_fzf
 bindkey '^g' search_ghq_with_fzf
 
-# Ctrl-w: Search Tmux with Fzf
-zle -N search_tmux
-bindkey '^w' search_tmux
