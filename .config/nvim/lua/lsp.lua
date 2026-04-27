f5bf8a004d969f66a6d3295e8eb0695ab6961c16
Ryo Igarashi <n33t5hin@gmail.com>
2026-04-27 10:25:08 +0900

    fix: Use :LspEslintFixAll command

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,36 +1,38 @@
+vim.lsp.enable({
+    "ansiblels",
+    "lua_ls",
+    "ts_ls",
+    "eslint",
+    "ruby_lsp",
+    "pylsp",
+    "rust_analyzer",
+})
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/eslint.lua
 local base_on_attach = vim.lsp.config.eslint.on_attach
 vim.lsp.config("eslint", {
     on_attach = function(client, bufnr)
         if not base_on_attach then return end
         base_on_attach(client, bufnr)
-
-        -- 下記がリリースされたらドキュメントに記載のある方法に戻す
-        -- https://github.com/neovim/nvim-lspconfig/pull/4218
         vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = bufnr,
-            callback = function()
-                client:request_sync("workspace/executeCommand", {
-                    command = "eslint.applyAllFixes",
-                    arguments = {
-                        {
-                            uri = vim.uri_from_bufnr(bufnr),
-                            version = vim.lsp.util.buf_versions[bufnr],
-                        },
-                    },
-                }, nil, bufnr)
-            end,
+            command = "LspEslintFixAll",
         })
     end,
 })
 
-
-vim.lsp.enable("ansiblels")
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("ts_ls")
-vim.lsp.enable("eslint")
-vim.lsp.enable("ruby_lsp")
-vim.lsp.enable("pylsp");
-vim.lsp.enable("rust_analyzer");
+-- https://neovim.io/doc/user/lua/#vim.filetype.add()
+vim.filetype.add({
+    pattern = {
+        [".*%.ya?ml"] = function(path, bufnr)
+            -- https://neovim.io/doc/user/lua/#vim.fs.root()
+            local root = vim.fs.root(bufnr, { "ansible.cfg" })
+            if root then
+                return "yaml.ansible"
+            end
+        end,
+    },
+})
 
 -- https://github.com/stevearc/conform.nvim
 local js_formatters = { "prettierd", "prettier", stop_after_first = true }
@@ -63,18 +65,4 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function(args)
         require("conform").format({ bufnr = args.buf })
     end,
-})
-
--- https://neovim.io/doc/user/lua/#vim.filetype.add()
-vim.filetype.add({
-    -- https://neovim.io/doc/user/luaref/#lua-pattern
-    pattern = {
-        [".*%.ya?ml"] = function(path, bufnr)
-            -- https://neovim.io/doc/user/lua/#vim.fs.root()
-            local root = vim.fs.root(bufnr, { "ansible.cfg" })
-            if root then
-                return "yaml.ansible"
-            end
-        end,
-    },
 })
