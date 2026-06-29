001b974f0510b206c57e0b1b2d061d0142f50e46
Ryo Igarashi <n33t5hin@gmail.com>
2026-06-29 09:44:56 +0900

    feat: Add YankPathGh user command

--- .config/nvim/lua/user_command.lua
+++ .config/nvim/lua/user_command.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_user_command(
 )
 
 vim.api.nvim_create_user_command(
-    "YankAbsolutePath",
+    "YankPathAbs",
     function()
         local filepath = vim.fn.expand("%:p")
         vim.fn.setreg("+", filepath)
@@ -20,5 +20,29 @@ vim.api.nvim_create_user_command(
     end,
     {
         desc = "現在のファイルの絶対パスをコピーします"
+    }
+)
+
+function exec(command)
+    local handle = io.popen(command)
+    local result = handle:read("*a")
+    handle:close()
+    return result
+end
+
+vim.api.nvim_create_user_command(
+    "YankPathGh",
+    function()
+        local origin = exec("git remote get-url origin")
+        local base_url = string.sub(origin, 1, -6);
+        local branch = exec("git branch --show-current")
+        local filepath = vim.fn.expand("%")
+
+        local parmalink = string.format("%s/blob/%s/%s", base_url, branch, filepath);
+        vim.fn.setreg("+", parmalink)
+        vim.notify(parmalink .. " has been copied")
+    end,
+    {
+        desc = "現在のファイルのGitHubにおけるパーマリンクを取得します",
     }
 )
