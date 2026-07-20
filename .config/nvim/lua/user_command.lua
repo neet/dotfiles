26f78d76b40d454c1087e6e3e9908d5e5d7499a4
Ryo Igarashi <n33t5hin@gmail.com>
2026-07-20 23:59:52 +0900

    fix: Fix broken YankPath

--- .config/nvim/lua/user_command.lua
+++ .config/nvim/lua/user_command.lua
@@ -1,8 +1,18 @@
+local function exec(command)
+    local handle = io.popen(command)
+    if handle == nil then
+        return ""
+    end
+    local result = handle:read("*a")
+    handle:close()
+    return (result:gsub("%s+$", ""))
+end
+
 vim.api.nvim_create_user_command(
     "YankPath",
     function()
-        -- https://neovim.io/doc/user/vimfn/#expand()
-        local filepath = vim.fn.expand("%")
+        local abs_filepath = vim.fs.abspath(vim.api.nvim_buf_get_name(0))
+        local filepath = vim.fs.relpath(vim.fn.getcwd(), abs_filepath) or abs_filepath
         vim.fn.setreg("+", filepath)
         vim.notify(filepath .. " has been copied")
     end,
@@ -14,7 +24,7 @@ vim.api.nvim_create_user_command(
 vim.api.nvim_create_user_command(
     "YankPathAbs",
     function()
-        local filepath = vim.fn.expand("%:p")
+        local filepath = vim.fs.abspath(vim.api.nvim_buf_get_name(0))
         vim.fn.setreg("+", filepath)
         vim.notify(filepath .. " has been copied")
     end,
@@ -23,24 +33,21 @@ vim.api.nvim_create_user_command(
     }
 )
 
-function exec(command)
-    local handle = io.popen(command)
-    local result = handle:read("*a")
-    handle:close()
-    return result
-end
-
 vim.api.nvim_create_user_command(
     "YankPathGh",
     function()
-        local origin = exec("git remote get-url origin")
-        local base_url = string.sub(origin, 1, -6);
-        local branch = exec("git branch --show-current")
-        local filepath = vim.fn.expand("%")
+        local abs_filepath = vim.fs.abspath(vim.api.nvim_buf_get_name(0))
+        local filedir = vim.fn.shellescape(vim.fs.dirname(abs_filepath))
 
-        local parmalink = string.format("%s/blob/%s/%s", base_url, branch, filepath);
-        vim.fn.setreg("+", parmalink)
-        vim.notify(parmalink .. " has been copied")
+        local origin = exec("git -C " .. filedir .. " remote get-url origin")
+        local base_url = origin:gsub("%.git$", "")
+        local branch = exec("git -C " .. filedir .. " branch --show-current")
+        local root = exec("git -C " .. filedir .. " rev-parse --show-toplevel")
+        local relpath = vim.fs.relpath(root, abs_filepath)
+
+        local permalink = string.format("%s/blob/%s/%s", base_url, branch, relpath)
+        vim.fn.setreg("+", permalink)
+        vim.notify(permalink .. " has been copied")
     end,
     {
         desc = "現在のファイルのGitHubにおけるパーマリンクを取得します",
