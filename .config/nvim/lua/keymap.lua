0069dc069a9df08a3194062914dd04fb62620d00
Ryo Igarashi <n33t5hin@gmail.com>
2026-07-25 14:12:08 +0900

    chore: Use neovim default keybindings for LSP

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,26 +1,24 @@
 -- https://neovim.io/doc/user/lsp/#_lua-module%3a-vim.lsp.buf
 vim.keymap.set("n", "<leader>pr", function()
-    vim.lsp.buf.references(nil)
+    vim.notify("Use grr instead")
 end)
 
 vim.keymap.set("n", "<leader>pd", function()
-    vim.lsp.buf.definition()
+    vim.notify("Use <C-]> instead")
 end)
 
-vim.keymap.set("n", "<leader>pe", vim.diagnostic.open_float, {
-    desc = "Peek Errors",
-})
+vim.keymap.set("n", "<leader>pe", function()
+    vim.notify("Use <C-w>d instead")
+end)
 
-vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {
-    desc = "LSP Rename symbol",
-})
+vim.keymap.set("n", "<F2>", function()
+    vim.notify("Use grn instead")
+end)
 
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
-    desc = "Code Action",
-})
+vim.keymap.set("n", "<leader>ca", function()
+    vim.notify("Use gra instead")
+end)
 
-vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", {
-    silent = true
-})
-
-vim.keymap.set("n", "<leader>qf", vim.diagnostic.setqflist)
+vim.keymap.set("n", "<leader>e", function()
+    vim.cmd("Oil")
+end, { silent = true })
