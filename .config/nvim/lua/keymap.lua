e8fc12fddd31f51d50a2b2811aa5db9471d7dab2
Ryo Igarashi <n33t5hin@gmail.com>
2026-09-20 00:45:21 +0900

    chore: Remove unused Neovim keymap

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,24 +1,4 @@
 -- https://neovim.io/doc/user/lsp/#_lua-module%3a-vim.lsp.buf
-vim.keymap.set("n", "<leader>pr", function()
-    vim.notify("Use grr instead")
-end)
-
-vim.keymap.set("n", "<leader>pd", function()
-    vim.notify("Use <C-]> instead")
-end)
-
-vim.keymap.set("n", "<leader>pe", function()
-    vim.notify("Use <C-w>d instead")
-end)
-
-vim.keymap.set("n", "<F2>", function()
-    vim.notify("Use grn instead")
-end)
-
-vim.keymap.set("n", "<leader>ca", function()
-    vim.notify("Use gra instead")
-end)
-
 vim.keymap.set("n", "<leader>e", function()
     vim.cmd("Oil")
 end, { silent = true })
